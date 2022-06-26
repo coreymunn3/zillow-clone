b2ca40505ce93f4c3fc3b0bd20252ade357c1fbe
Michael Corey Munn <coreymunn3@gmail.com>
2022-06-26 15:40:53 -0400

    Setup named export system for components

---
+++ components/Banner/Banner.jsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Flex, Box, Text, Button } from '@chakra-ui/react';
+
+export const Banner = ({
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  linkName,
+  imageUrl,
+  buttonText,
+}) => {
+  return (
+    <Flex justifyContent={'center'} alignItems='center' m='10'>
+      <Image src={imageUrl} width={500} height={300} />
+      <Box p={5}>
+        <Text color={'gray.500'} fontSize='small' fontWeight='medium'>
+          {purpose}
+        </Text>
+        <Text fontSize='3xl' fontWeight='medium'>
+          {title1} <br /> {title2}
+        </Text>
+        <Text color='gray.700' fontSize='lg' pt={3} pb={3} fontWeight='medium'>
+          {desc1} <br /> {desc2}
+        </Text>
+        <Button fontSize={'xl'} bg='blue.300' color='white'>
+          <Link href={linkName}>{buttonText}</Link>
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
